162c6448d5e16a214b2e472e0a0a2d99a3ffed60
gaev-art <forsakensr@gmail.com>
2020-09-25 14:50:00 +0300

    Updates

---
+++ precache-manifest.a466bde6a43db9614cce518e873e8345.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7423b35ed5172e9c0eef6cb7f9c40304",
+    "url": "/snap_chat/index.html"
+  },
+  {
+    "revision": "686917444604ce71a3e8",
+    "url": "/snap_chat/static/css/main.df4f7932.chunk.css"
+  },
+  {
+    "revision": "7ec4ff77a0dac5972e37",
+    "url": "/snap_chat/static/js/2.741dbed9.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/snap_chat/static/js/2.741dbed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "686917444604ce71a3e8",
+    "url": "/snap_chat/static/js/main.94eb601d.chunk.js"
+  },
+  {
+    "revision": "8a2a014efc3e689babec",
+    "url": "/snap_chat/static/js/runtime-main.48ca1a91.js"
+  }
+]);
